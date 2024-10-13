6dfc8ea0d0ae449e3621d58690c27d766a0785b3
avraham <2721avi@gmail.com>
2024-10-13 14:49:12 +0300

    add registerTeacher

--- src/services/teecherService.ts
+++ src/services/teecherService.ts
@@ -1,10 +1,28 @@
 import { ITeacher } from "../models/teecheModel";
 import { authDto } from "../types/authDto";
-
-export const register = async (teacher: ITeacher) => {
+import Class from "../models/classModel";
+export const registerTeacher = async (teacher: ITeacher, className: string) => {
   try {
+    const existingClass = await Class.findOne({ name: className });
+    if (existingClass) {
+      throw new Error(
+        "Class name already exists. Please choose a different name."
+      );
+    }
+
+    const newClass = new Class({
+      name: className,
+      teacher: teacher._id,
+      students: [],
+    });
+
+    await newClass.save();
+
+    await teacher.save();
+    return teacher;
   } catch (err) {
     console.log(err);
+    throw new Error("Failed to register teacher and create class.");
   }
 };
 
